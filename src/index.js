aec4990a4e9fba32951dd349616fbd25588bc1b9
Nikitaiskorka <nikitagerassimenko@gmail.com>
2021-05-13 16:41:56 +0200

    сделал отрисовку элементов

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import './js/fetchCountries'
 import countryCard from "./js/templates/country-card.hbs"
 import fetchCountries from './js/fetchCountries'
 import notification from './js/pnotify'
-
+var debounce = require('lodash.debounce');
+console.dir(debounce);
 const refs = {
     input: document.querySelector('.input-js'),
     countryContainer: document.querySelector('.country-list')
@@ -11,14 +12,36 @@ const refs = {
 console.log(refs.input);
 console.log(refs.countryContainer);
 
-refs.input.addEventListener('input',onInputChange)
+refs.input.addEventListener('input',  debounce(onInputChange,100)) 
+
 function onInputChange(event) {
+    clearCountryList()
     const inputValue = event.currentTarget.value.toLowerCase()
-    fetchCountries(inputValue).then(country => {
-         render(country)
-     })
+    fetchCountries(inputValue).then(makeCountryCardMarkup).then(insertCountryCardMarkup).catch(notification.onErrorNotification)
 }
-function render(country) {
-    const markup = countryCard(country)
-    refs.countryContainer.insertAdjacentHTML('beforeend',markup)
+
+
+function makeCountryCardMarkup(country) {
+    if (country.length > 10) {
+        notification.onTooManyResults()
+        return ''
+    }
+    if (country.length === 1) {
+        notification.onSucsessNotification()
+       return  countryCard(country[0])
+    }
+    
+    
+    return countryCard(country)
 }
+
+
+function insertCountryCardMarkup(markup) {
+    refs.countryContainer.insertAdjacentHTML('beforeend', markup)
+    
+}
+
+function clearCountryList() {
+    refs.countryContainer.innerHTML =''
+}
+
