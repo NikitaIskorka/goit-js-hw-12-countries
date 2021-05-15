bb8d533a0db3ddebfc52c5a3abd84f8f9d200921
Nikitaiskorka <nikitagerassimenko@gmail.com>
2021-05-15 09:19:11 +0200

    1

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://restcountries.eu/rest/v2/name/'
 function fetchCountries(searchQuery) {
-    return fetch(baseUrl + `${searchQuery}`).then(response => {
+    return fetch(baseUrl + searchQuery).then(response => {
         return response.json()
     })
 }
